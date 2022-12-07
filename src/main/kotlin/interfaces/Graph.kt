677022d12138380c5887f07d143bd42547e04cea
icestormikk <jigalow.pavel2015@yandex.ru>
2022-12-07 15:32:45 +0300

    Definition of a function for copying two-dimensional arrays

--- src/main/kotlin/interfaces/Graph.kt
+++ src/main/kotlin/interfaces/Graph.kt
@@ -5,11 +5,15 @@ import domain.Vertex
 interface Graph<T> {
     val adjVertices: MutableMap<Vertex, *>
 
-    fun buildGraph(vararg edges: T)
     fun printGraph()
-    fun getVertexByID(vertexID: Long) : Vertex =
+    fun getVertexByID(vertexID: Long): Vertex =
         adjVertices.keys.firstOrNull() { it.id == vertexID }
             ?: throw IllegalArgumentException("Vertex with id $vertexID does not exists")
-    fun getVertexOrNull(vertexID: Long) : Vertex? =
+
+    fun getVertexOrNull(vertexID: Long): Vertex? =
         adjVertices.keys.firstOrNull { it.id == vertexID }
+
 }
+
+inline fun <reified T> Array<Array<T>>.copy(): Array<Array<T>> =
+    Array(size) { get(it).clone() }
